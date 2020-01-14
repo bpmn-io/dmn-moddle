bed3500e65ddc36f058256a35981fdd25961b090
Philipp Fromme <philippfromme@outlook.com>
2020-01-14 17:23:28 +0100

    chore(tests): refactor

--- test/spec/xml/read.js
+++ test/spec/xml/read.js
@@ -1,59 +1,135 @@
+import fs from 'fs';
+
 import expect from '../../expect';
 
-import {
-  createModdle,
-  readFile
-} from '../../helper';
+import DmnModdle from '../../../lib';
 
 
 describe('dmn-moddle - read', function() {
 
-  const moddle = createModdle();
+  let moddle;
 
-  function read(xml, root, options, callback) {
-    return moddle.fromXML(xml, root, options, callback);
-  }
+  beforeEach(function() {
+    moddle = new DmnModdle();
+  });
 
-  function fromFile(file, root, options, callback) {
-    const contents = readFile(file);
+  async function read(fileName, root = 'dmn:Definitions') {
+    return new Promise((resolve, reject) => {
+      const file = fs.readFileSync(fileName, 'utf8');
 
-    return read(contents, root, options, callback);
+      moddle.fromXML(file, root, (err, definitions) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(definitions);
+      });
+    });
   }
 
 
   describe('DMN', function() {
 
-    it('Decision', function(done) {
+    it('Decision', async function() {
+
+      // given
+      const expected = {
+        $type: 'dmn:Decision',
+        id: 'Decision_1',
+        name: 'Decision'
+      };
 
       // when
-      fromFile('test/fixtures/dmn/decision.part.dmn', 'dmn:Definitions', function(err, result) {
+      const definitions = await read('test/fixtures/dmn/dmn/decision.dmn');
 
-        const expected = {
-          $type: 'dmn:Decision',
-          id: 'Decision_1',
-          name: 'Decision'
-        };
-
-        // then
-        expect(err).not.to.exist;
-        expect(result.drgElement[ 0 ]).to.jsonEqual(expected);
-
-        done(err);
-      });
+      // then
+      expect(definitions.get('drgElement')[ 0 ]).to.jsonEqual(expected);
     });
 
 
-    it('InputData');
+    it('InputData', async function() {
+
+      // given
+      const expected = {
+        $type: 'dmn:InputData',
+        id: 'InputData_1',
+        name: 'InputData'
+      };
+
+      // when
+      const definitions = await read('test/fixtures/dmn/dmn/input-data.dmn');
+
+      // then
+      expect(definitions.get('drgElement')[ 0 ]).to.jsonEqual(expected);
+    });
 
   });
 
 
   describe('DI', function() {
 
-    it('Decision');
+    it('Decision & DI', async function() {
+
+      // given
+      const expected = {
+        $type: 'dmndi:DMNDI',
+        diagrams: [
+          {
+            $type: 'dmndi:DMNDiagram',
+            diagramElements: [
+              {
+                $type: 'dmndi:DMNShape',
+                bounds: {
+                  $type: 'dc:Bounds',
+                  height: 80,
+                  width: 180,
+                  x: 100,
+                  y: 100
+                }
+              }
+            ]
+          }
+        ]
+      };
+
+      // when
+      const definitions = await read('test/fixtures/dmn/dmndi/decision.dmn');
+
+      // then
+      expect(definitions.get('dmnDI')).to.jsonEqual(expected);
+    });
 
 
-    it('InputData');
+    it('InputData', async function() {
+
+      // given
+      const expected = {
+        $type: 'dmndi:DMNDI',
+        diagrams: [
+          {
+            $type: 'dmndi:DMNDiagram',
+            diagramElements: [
+              {
+                $type: 'dmndi:DMNShape',
+                bounds: {
+                  $type: 'dc:Bounds',
+                  height: 45,
+                  width: 125,
+                  x: 100,
+                  y: 100
+                }
+              }
+            ]
+          }
+        ]
+      };
+
+      // when
+      const definitions = await read('test/fixtures/dmn/dmndi/input-data.dmn');
+
+      // then
+      expect(definitions.get('dmnDI')).to.jsonEqual(expected);
+    });
 
   });
 
--- test/spec/xml/write.js
+++ test/spec/xml/write.js
@@ -1,41 +1,55 @@
 import expect from '../../expect';
 
-import { createModdle } from '../../helper';
+import DmnModdle from '../../../lib';
 
 
 describe('dmn-moddle - write', function() {
 
-  const moddle = createModdle();
+  let moddle;
 
-  function write(element, options, callback) {
-    moddle.toXML(element, options, callback);
+  beforeEach(function() {
+    moddle = new DmnModdle();
+  });
+
+  function write(element, options = { preamble: false }) {
+    return new Promise((resolve, reject) => {
+      moddle.toXML(element, options, (err, xml) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(xml);
+      });
+    });
   }
 
 
   describe('dmn', function() {
 
-    it('Definitions', function(done) {
+    it('Definitions', async function() {
 
       // given
-      const definitions = moddle.create('dmn:Definitions');
-
       const expected = '<dmn:definitions xmlns:dmn="https://www.omg.org/spec/DMN/20191111/MODEL/" />';
 
+      const definitions = moddle.create('dmn:Definitions');
+
       // when
-      write(definitions, { preamble: false }, function(err, result) {
+      const xml = await write(definitions);
 
-        // then
-        expect(result).to.eql(expected);
-
-        done(err);
-      });
-
+      // then
+      expect(xml).to.equal(expected);
     });
 
 
-    it('LiteralExpression', function(done) {
+    it('LiteralExpression', async function() {
 
       // given
+      const expected =
+        '<dmn:decision xmlns:dmn="https://www.omg.org/spec/DMN/20191111/MODEL/" id="Decision_1" name="Foo">' +
+          '<dmn:variable id="InformationItem_1" name="Bar" typeRef="integer" />' +
+          '<dmn:literalExpression id="LiteralExpression_1" />' +
+        '</dmn:decision>';
+
       const informationItem = moddle.create('dmn:InformationItem', {
         id: 'InformationItem_1',
         name: 'Bar',
@@ -53,20 +67,11 @@ describe('dmn-moddle - write', function() {
         variable: informationItem
       });
 
-      const expected =
-        '<dmn:decision xmlns:dmn="https://www.omg.org/spec/DMN/20191111/MODEL/" id="Decision_1" name="Foo">' +
-        '<dmn:variable id="InformationItem_1" name="Bar" typeRef="integer" />' +
-        '<dmn:literalExpression id="LiteralExpression_1" />' +
-        '</dmn:decision>';
-
       // when
-      write(decision, { preamble: false }, function(err, result) {
+      const xml = await write(decision);
 
-        // then
-        expect(result).to.eql(expected);
-
-        done(err);
-      });
+      // then
+      expect(xml).to.equal(expected);
     });
 
   });
@@ -74,10 +79,140 @@ describe('dmn-moddle - write', function() {
 
   describe('di', function() {
 
-    it('Decision');
+    it('Decision', async function() {
+
+      // given
+      const expected =
+        '<dmn:definitions xmlns:dmn="https://www.omg.org/spec/DMN/20191111/MODEL/" xmlns:dmndi="https://www.omg.org/spec/DMN/20191111/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/">' +
+          '<dmn:decision id="Decision_1" name="Decision_1" />' +
+          '<dmndi:DMNDI>' +
+            '<dmndi:DMNDiagram id="DMNDiagram_1">' +
+              '<dmndi:DMNShape di:id="DMNShape_1" dmnElementRef="Decision_1">' +
+                '<dc:Bounds height="80" width="180" x="100" y="100" />' +
+              '</dmndi:DMNShape>' +
+            '</dmndi:DMNDiagram>' +
+          '</dmndi:DMNDI>' +
+        '</dmn:definitions>';
+
+      const definitions = moddle.create('dmn:Definitions');
+
+      const decision = moddle.create('dmn:Decision', {
+        id: 'Decision_1',
+        name: 'Decision_1'
+      });
+
+      definitions.get('drgElement').push(decision);
+
+      decision.$parent = definitions;
+
+      const dmnDiagram = moddle.create('dmndi:DMNDiagram', {
+        id: 'DMNDiagram_1',
+        diagramElements: []
+      });
+
+      const dmnDI = moddle.create('dmndi:DMNDI', {
+        diagrams: [ dmnDiagram ]
+      });
+
+      dmnDiagram.$parent = dmnDI;
+
+      definitions.set('dmnDI', dmnDI);
+
+      dmnDI.$parent = definitions;
+
+      const bounds = moddle.create('dc:Bounds', {
+        height: 80,
+        width: 180,
+        x: 100,
+        y: 100
+      });
+
+      const shape = moddle.create('dmndi:DMNShape', {
+        id: 'DMNShape_1',
+        bounds,
+        dmnElementRef: decision
+      });
+
+      bounds.$parent = shape;
+
+      dmnDiagram.get('diagramElements').push(shape);
+
+      shape.$parent = dmnDiagram.get('diagramElements');
+
+      // when
+      const xml = await write(definitions);
+
+      // then
+      expect(xml).to.equal(expected);
+    });
 
 
-    it('InputData');
+    it('InputData', async function() {
+
+      // given
+      const expected =
+        '<dmn:definitions xmlns:dmn="https://www.omg.org/spec/DMN/20191111/MODEL/" xmlns:dmndi="https://www.omg.org/spec/DMN/20191111/DMNDI/" xmlns:di="http://www.omg.org/spec/DMN/20180521/DI/" xmlns:dc="http://www.omg.org/spec/DMN/20180521/DC/">' +
+          '<dmn:inputData id="InputData_1" name="InputData_1" />' +
+          '<dmndi:DMNDI>' +
+            '<dmndi:DMNDiagram id="DMNDiagram_1">' +
+              '<dmndi:DMNShape di:id="DMNShape_1" dmnElementRef="InputData_1">' +
+                '<dc:Bounds height="45" width="125" x="100" y="100" />' +
+              '</dmndi:DMNShape>' +
+            '</dmndi:DMNDiagram>' +
+          '</dmndi:DMNDI>' +
+        '</dmn:definitions>';
+
+      const definitions = moddle.create('dmn:Definitions');
+
+      const inputData = moddle.create('dmn:InputData', {
+        id: 'InputData_1',
+        name: 'InputData_1'
+      });
+
+      definitions.get('drgElement').push(inputData);
+
+      inputData.$parent = definitions;
+
+      const dmnDiagram = moddle.create('dmndi:DMNDiagram', {
+        id: 'DMNDiagram_1',
+        diagramElements: []
+      });
+
+      const dmnDI = moddle.create('dmndi:DMNDI', {
+        diagrams: [ dmnDiagram ]
+      });
+
+      dmnDiagram.$parent = dmnDI;
+
+      definitions.set('dmnDI', dmnDI);
+
+      dmnDI.$parent = definitions;
+
+      const bounds = moddle.create('dc:Bounds', {
+        height: 45,
+        width: 125,
+        x: 100,
+        y: 100
+      });
+
+      const shape = moddle.create('dmndi:DMNShape', {
+        id: 'DMNShape_1',
+        bounds,
+        dmnElementRef: inputData
+      });
+
+      bounds.$parent = shape;
+
+      dmnDiagram.get('diagramElements').push(shape);
+
+      shape.$parent = dmnDiagram.get('diagramElements');
+
+      // when
+      const xml = await write(definitions);
+
+      // then
+      expect(xml).to.equal(expected);
+    });
 
   });
 
