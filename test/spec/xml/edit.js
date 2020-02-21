4b09939f7b164c16e18635ce5be46e7e3c9119e5
Nico Rehwaldt <git_nikku@nixis.de>
2020-02-21 19:20:22 +0100

    fix(model): correct dmn:ItemDefinition

--- test/spec/xml/edit.js
+++ test/spec/xml/edit.js
@@ -4,6 +4,9 @@ import expect from '../../expect';
 
 import DmnModdle from '../../../lib';
 
+import { validateXML } from 'xsd-schema-validator';
+
+const xsd = 'resources/dmn/xsd/DMN13.xsd';
 
 describe('dmn-moddle - edit', function() {
 
@@ -39,6 +42,19 @@ describe('dmn-moddle - edit', function() {
     });
   }
 
+  function validate(xml) {
+
+    return new Promise((resolve, reject) => {
+      validateXML(xml, xsd, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(result);
+      });
+    });
+
+  }
 
   describe('DMN', function() {
 
@@ -59,6 +75,69 @@ describe('dmn-moddle - edit', function() {
 
       // then
       expect(xml).to.equal(expected);
+    });
+
+
+    it('create ItemDefinition', async function() {
+
+      const def_1 = moddle.create('dmn:ItemDefinition', {
+        typeLanguage: 'FEEL',
+        name: 'def_1',
+        typeRef: 'xsd:string',
+        allowedValues: moddle.create('dmn:UnaryTests', {
+          text: 'a, b'
+        })
+      });
+
+      const def_2_component = moddle.create('dmn:ItemDefinition', {
+        typeLanguage: 'FEEL',
+        name: 'def_2_component',
+        typeRef: 'xsd:string',
+        allowedValues: moddle.create('dmn:UnaryTests', {
+          text: '[0..5]'
+        })
+      });
+
+      const def_2 = moddle.create('dmn:ItemDefinition', {
+        name: 'def_2',
+        itemComponent: [
+          def_2_component
+        ],
+        isCollection: true
+      });
+
+      const def_3 = moddle.create('dmn:ItemDefinition', {
+        name: 'def_3',
+        functionItem: moddle.create('dmn:FunctionItem', {
+          parameters: [
+            moddle.create('dmn:InformationItem', {
+              typeRef: 'string',
+              name: 'ProductType'
+            }),
+            moddle.create('dmn:InformationItem', {
+              typeRef: 'number',
+              name: 'Rate'
+            })
+          ],
+          outputTypeRef: 'xsi:string'
+        })
+      });
+
+      const definitions = moddle.create('dmn:Definitions', {
+        name: 'foo',
+        namespace: 'http://foo',
+        itemDefinition: [
+          def_1,
+          def_2,
+          def_3
+        ]
+      });
+
+      // when
+      const xml = await write(definitions);
+
+      // then
+      await validate(xml);
     });
 
   });
