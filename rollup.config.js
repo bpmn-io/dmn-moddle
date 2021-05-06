0d04b10f5e8a157a579412ac861d602be0752cc6
Max Tru <maximilian.trumpf@camunda.com>
2021-05-06 14:01:57 +0200

    deps: bumps devDependencies and resolve security audit warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { terser } from 'rollup-plugin-terser';
 
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
