0bac84d6e8a9a8137e0956d4e310673620ef4037
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-07-22 13:18:58 +0200

    chore: use built-in eslint extension for `with-json`
    Co-authored-by: Maciej Barelkowski <28307541+barmac@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
 
-import babelParser from '@babel/eslint-parser';
-
 const files = {
   build: [
-    'tasks/**/*.cjs'
+    'tasks/**/*.cjs',
+    '*.js'
   ],
   test: [
     'test/**/*.js',
@@ -48,22 +47,10 @@ export default [
     };
   }),
 
-  // other
-  // hook up babel parser
+  // support "with" import
   {
-    files: [ '**/*.js', '**/*.mjs' ],
     languageOptions: {
-      parser: babelParser,
-      parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          babelrc: false,
-          configFile: false,
-          plugins: [
-            '@babel/plugin-syntax-import-attributes'
-          ]
-        },
-      }
+      ecmaVersion: 2025
     }
   }
 ];
