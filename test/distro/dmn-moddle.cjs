845ff99d6495c02443b1dc58baa1c79859d3c14e
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-19 16:42:23 +0100

    chore!: expose DmnModdle as named export
    Instead of exposing <default>, expose <DmnModdle> named.
    Improves CJS / ESM compatibility.

--- test/distro/dmn-moddle.cjs
+++ test/distro/dmn-moddle.cjs
@@ -4,14 +4,14 @@ const { expect } = require('chai');
 describe('dmn-moddle', function() {
 
   it('should expose ESM bundle', async function() {
-    const DmnModdle = await import('dmn-moddle').then(m => m.default);
+    const { DmnModdle } = await import('dmn-moddle');
 
     expect(new DmnModdle()).to.exist;
   });
 
 
   it('should export CJS bundle', function() {
-    const DmnModdle = require('dmn-moddle');
+    const { DmnModdle } = require('dmn-moddle');
 
     expect(new DmnModdle()).to.exist;
   });
